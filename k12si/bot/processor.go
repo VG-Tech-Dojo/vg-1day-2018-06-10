99c31b7da1b7d8784c7af236ba555529cbf5daa3
k12si <kakushin.nishimura@nislab.doshisha.ac.jp>
2018-06-10 11:37:24 +0900

    8080接続ok

---
+++ k12si/bot/processor.go
@@ -0,0 +1,84 @@
+package bot
+
+import (
+	"regexp"
+	"strings"
+
+	"fmt"
+
+	"github.com/VG-Tech-Dojo/vg-1day-2018-06-10/k12si/env"
+	"github.com/VG-Tech-Dojo/vg-1day-2018-06-10/k12si/model"
+	"net/url"
+)
+
+const (
+	keywordAPIURLFormat = "https://jlp.yahooapis.jp/KeyphraseService/V1/extract?appid=%s&sentence=%s&output=json"
+)
+
+type (
+	// Processor はmessageを受け取り、投稿用messageを作るインターフェースです
+	Processor interface {
+		Process(message *model.Message) (*model.Message, error)
+	}
+
+	// HelloWorldProcessor は"hello, world!"メッセージを作るprocessorの構造体です
+	HelloWorldProcessor struct{}
+
+	// OmikujiProcessor は"大吉", "吉", "中吉", "小吉", "末吉", "凶"のいずれかをランダムで作るprocessorの構造体です
+	OmikujiProcessor struct{}
+
+	// KeywordProcessor はメッセージ本文からキーワードを抽出するprocessorの構造体です
+	KeywordProcessor struct{}
+)
+
+// Process は"hello, world!"というbodyがセットされたメッセージのポインタを返します
+func (p *HelloWorldProcessor) Process(msgIn *model.Message) (*model.Message, error) {
+	return &model.Message{
+		Body: msgIn.Body + ", world!",
+	}, nil
+}
+
+// Process は"大吉", "吉", "中吉", "小吉", "末吉", "凶"のいずれかがbodyにセットされたメッセージへのポインタを返します
+func (p *OmikujiProcessor) Process(msgIn *model.Message) (*model.Message, error) {
+	fortunes := []string{
+		"大吉",
+		"吉",
+		"中吉",
+		"小吉",
+		"末吉",
+		"凶",
+	}
+	result := fortunes[randIntn(len(fortunes))]
+	return &model.Message{
+		Body: result,
+	}, nil
+}
+
+// Process はメッセージ本文からキーワードを抽出します
+func (p *KeywordProcessor) Process(msgIn *model.Message) (*model.Message, error) {
+	r := regexp.MustCompile("\\Akeyword (.+)")
+	matchedStrings := r.FindStringSubmatch(msgIn.Body)
+	if len(matchedStrings) != 2 {
+		return nil, fmt.Errorf("bad message: %s", msgIn.Body)
+	}
+
+	text := matchedStrings[1]
+
+	requestURL := fmt.Sprintf(keywordAPIURLFormat, env.KeywordAPIAppID, url.QueryEscape(text))
+
+	type keywordAPIResponse map[string]interface{}
+	var response keywordAPIResponse
+	get(requestURL, &response)
+
+	keywords := make([]string, 0, len(response))
+	for k, v := range response {
+		if k == "Error" {
+			return nil, fmt.Errorf("%#v", v)
+		}
+		keywords = append(keywords, k)
+	}
+
+	return &model.Message{
+		Body: "キーワード：" + strings.Join(keywords, ", "),
+	}, nil
+}
